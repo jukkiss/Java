f060c0e1a7f0ade5b164bb18b2f50630199a3e7e
Jukka <savela.j@gmail.com>
2024-03-22 09:42:59 +0200

    "new version"

--- fundamentals-of-java/FahrenheitToCelsius/src/FahrenheitToCelsius.java
+++ fundamentals-of-java/FahrenheitToCelsius/src/FahrenheitToCelsius.java
@@ -4,11 +4,8 @@ public class FahrenheitToCelsius {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        //Ask user degrees in Fahrenheit
         System.out.println("Give temperature in fahrenheit: ");
-        //Take the input
         int fahr = Integer.parseInt(scanner.nextLine());
-        //Convert the input to Celsius and print the result
         double cels = (fahr - 32) / 1.8;
         String formattedCels = String.format("%.1f", cels);
         System.out.println("Temperature in Celsius: " + formattedCels);
