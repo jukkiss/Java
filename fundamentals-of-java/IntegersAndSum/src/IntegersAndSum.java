acc27143d4bbe015ea6c56eb1ab0b9835c47e557
Jukka <savela.j@gmail.com>
2024-03-22 09:24:17 +0200

    "new version"

--- fundamentals-of-java/IntegersAndSum/src/IntegersAndSum.java
+++ fundamentals-of-java/IntegersAndSum/src/IntegersAndSum.java
@@ -1,25 +1,18 @@
 import java.util.Scanner;
-
 public class IntegersAndSum {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Give the first number:");
+        System.out.println("Give the first number: ");
         int first = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Give the second number:");
+        System.out.println("Give the second number: ");
         int second = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Give the third number:");
+        System.out.println("Give the third number: ");
         int third = Integer.parseInt(scanner.nextLine());
 
-        int sum = first + second + third;
-        int product = first * second * third; //MAke a variable called product, get the product by using simple math
-        double average = sum / 3.0; // Using 3.0 to ensure floating point division
-
-        System.out.println("The sum of the numbers is " + sum);
-        System.out.println("The product of the numbers is " + product);
-        System.out.println("The average of the numbers is " + average);
+        System.out.println("The sum of numbers is: " + (first + second + third));
+        System.out.println("The product of the numbers is: " + (first * second * third));
+        System.out.println("The average of the numbers is: " + (first * second * third / 3));
     }
 }
